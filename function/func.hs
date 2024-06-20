f99d510307984792a80a5524e9da9df4cff2ecb7
boli <bipinoli90@gmail.com>
2024-06-20 12:02:44 +0200

    function

---
+++ function/func.hs
@@ -0,0 +1,35 @@
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_ : rest) = 1 + length' rest
+
+firstLetter :: String -> String
+firstLetter "" = error "empty string"
+firstLetter all@(x : xs) = "The first letter of '" ++ all ++ "' is " ++ [x]
+
+bmiIndex :: (RealFloat f) => f -> f -> String
+bmiIndex height weight
+  | bmi <= skinny = "underweight"
+  | bmi <= normal = "normal"
+  | bmi <= overweight = "overweight"
+  | otherwise = "morbidly overweight"
+  where
+    bmi = weight / height ^ 2
+    skinny = 18.5
+    normal = 25.0
+    overweight = 30.0
+
+maxFunc :: (Ord a) => a -> a -> a
+maxFunc x y
+  | x < y = y
+  | otherwise = x
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x : xs) =
+  let smaller = quicksort [k | k <- xs, k <= x]
+      bigger = quicksort [k | k <- xs, k > x]
+   in smaller ++ [x] ++ bigger
